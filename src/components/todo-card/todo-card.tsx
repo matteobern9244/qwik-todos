22f59ad0ac2d020421aaf8858f19332c4644b70c
Christian Massaro <christian.massaro4@gmail.com>
2023-05-11 00:16:09 +0200

    Restyled with tailwind css; Initial API for GET, DELETE and PUT

--- src/components/todo-card/todo-card.tsx
+++ src/components/todo-card/todo-card.tsx
@@ -1,30 +1,63 @@
 import { $, component$ } from "@builder.io/qwik";
-import "./todo-card.css";
+import type { PropFunction } from "@builder.io/qwik";
 import type { Todo } from "~/models/todo";
 
 interface Props {
   todo: Todo;
+  onDelete$: PropFunction<(id: number) => void>;
+  onUpdate$: PropFunction<(todo: Todo) => void>;
 }
 
-export const TodoCard = component$(({ todo }: Props) => {
-  const deleteNote = $((id: number) => {
-    console.log(`Todo with id ${id} deleted.`);
+export const TodoCard = component$(({ todo, onDelete$, onUpdate$ }: Props) => {
+  const deleteNote = $(async (id: number) => {
+    const deleted = await fetch(`http://localhost:3000/todos/${id}`, {
+      method: "DELETE",
+    });
+    if (deleted) {
+      onDelete$(id);
+    }
   });
-  const toggleNoteStatus = $((status: boolean) => {
-    console.log(`Changed todo status to ${status ? "Done" : "Todo"}.`);
+  const toggleNoteStatus = $(async (update: Todo) => {
+    const toUpdate: Todo = {
+      ...update,
+      done: !update.done,
+    };
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(toUpdate),
+    };
+    const updated = await fetch(
+      `http://localhost:3000/todos/${update.id}`,
+      requestOptions
+    );
+    const newTodo = await updated.json();
+    onUpdate$(newTodo);
   });
 
   return (
-    <div class="todo-card">
-      <span class={`description ${todo.done ? "done" : "todo"}`}>
+    <div class="bg-white shadow-xl p-4 grid rounded-md divide-y divide-dashed divide-slate-400">
+      <span class={`pb-4 text-lg ${todo.done && "line-through"}`}>
+        Todo #{todo.id}
+      </span>
+      <span class={`py-8 ${todo.done && "line-through"}`}>
         {todo.description}
       </span>
-      <div class="spacer"></div>
-      <div class="actions">
-        <button onClick$={() => toggleNoteStatus(!todo.done)}>
-          {todo.done ? "Mark as Todo" : "Mark as Done"}
+      <div class="pt-4 flex flex-row justify-end gap-4">
+        <button
+          class={`px-3 py-2 rounded-md text-white text-sm ${
+            todo.done ? "bg-cyan-600" : "bg-emerald-600"
+          }`}
+          onClick$={() => toggleNoteStatus(todo)}
+        >
+          {todo.done ? "Restore" : "Done"}
         </button>
-        <button onClick$={() => deleteNote(todo.id)}>Delete</button>
+        <button
+          class="px-3 py-2 rounded-md text-white text-sm bg-rose-600"
+          onClick$={() => deleteNote(todo.id)}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,40 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { $, component$, useSignal, useTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { TodoCard } from "~/components/todo-card/todo-card";
 import type { Todo } from "~/models/todo";
-import "./home.css";
-import { data } from "~/mock/data";
 
 export default component$(() => {
-  const todos = useSignal<Todo[]>(data);
+  const todos = useSignal<Todo[]>([]);
+
+  useTask$(async () => {
+    const data = await fetch("http://localhost:3000/todos");
+    todos.value = await data.json();
+  });
+
+  const onDelete$ = $((id: number) => {
+    todos.value = todos.value.filter((todo) => todo.id !== id);
+  });
+
+  const onUpdate$ = $((updated: Todo) => {
+    todos.value = todos.value.map((todo) =>
+      todo.id === updated.id ? updated : todo
+    );
+  });
+
   return (
-    <>
-      <h1>Todo List</h1>
-      <div class="todo-list">
+    <div class="grid gap-8 p-8">
+      <h1 class="text-4xl">Todo List</h1>
+      <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {todos.value.map((todo) => (
-          <TodoCard todo={todo} key={todo.id} />
+          <TodoCard
+            todo={todo}
+            key={todo.id}
+            onDelete$={(id: number) => onDelete$(id)}
+            onUpdate$={(todo: Todo) => onUpdate$(todo)}
+          />
         ))}
       </div>
-    </>
+    </div>
   );
 });
 
